41806427061486a7d5b0790a19e02236a5631a8f
David Tribugenia <david.estrelloso.tribugenia@gmail.com>
2024-06-19 18:00:49 +0800

    added: Search and Liked functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,12 +7,14 @@ import { HiHome } from "react-icons/hi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
+import { Song } from "@/types";
 
 interface SidebarProps {
   children: ReactNode;
+  songs: Song[];
 }
 
-const Sidebar = ({ children }: SidebarProps) => {
+const Sidebar = ({ children, songs }: SidebarProps) => {
   const pathname = usePathname();
 
   const routes = useMemo(() => {
@@ -42,7 +44,7 @@ const Sidebar = ({ children }: SidebarProps) => {
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
-          <Library />
+          <Library songs={songs} />
         </Box>
       </div>
       <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
